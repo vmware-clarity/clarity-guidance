24421a2cf956e6f9a84ed7f277e765bf8361606a
Kevin Buhmann <kbuhmann@vmware.com>
2024-10-21 08:19:13 -0500

    fixup: add not found page

--- guiance.clarity.design/src/app/app.routes.ts
+++ guiance.clarity.design/src/app/app.routes.ts
@@ -2,4 +2,11 @@ import { Routes } from '@angular/router';
 
 import { pageRoutes } from '../compiled-content/page-routes';
 
-export const routes: Routes = [...pageRoutes];
+export const routes: Routes = [
+  ...pageRoutes,
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./components/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent),
+  },
+];
