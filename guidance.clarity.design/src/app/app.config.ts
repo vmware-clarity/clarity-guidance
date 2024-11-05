847925c313e4ab4c196b89dc9fb139eefe51702e
Kevin Buhmann <kevin.buhmann@broadcom.com>
2024-11-05 09:48:37 -0600

    fix: scroll to top of `main` content after navigation (#57)

--- guidance.clarity.design/src/app/app.config.ts
+++ guidance.clarity.design/src/app/app.config.ts
@@ -1,9 +1,29 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, inject, Provider, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { NavigationEnd, provideRouter, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 import { routes } from './app.routes';
 
+const scrollTopOnNavigationProvider: Provider = {
+  provide: APP_INITIALIZER,
+  multi: true,
+  useFactory: () => {
+    const router = inject(Router);
+
+    return () => {
+      router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
+        document.querySelector('main')?.scrollTo(0, 0);
+      });
+    };
+  },
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations()],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    scrollTopOnNavigationProvider,
+  ],
 };
